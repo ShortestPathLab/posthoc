579df669f9e0b402188e9222ee4baeca90388a62
spaaaacccee <kaiwen530@gmail.com>
2023-11-30 18:19:16 +1100

    Write grid A-star tutorial

--- docs/2-get-visualiser.md
+++ docs/2-get-visualiser.md
@@ -4,4 +4,12 @@ sidebar_position: 2
 
 # Get Visualiser
 
-[TODO]
+Open the visualiser [here](https://path-visualiser.github.io/app/).
+
+## Install Visualiser as a Progressive Web App
+
+You can install the visualiser as a progressive web app, so that it runs in its own window.
+
+## Standalone Builds
+
+Check the [releases](https://github.com/path-visualiser/app/releases) page for a list of latest Windows and Linux releases.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 
 import { useEffect } from "react";
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-
   useEffect(() => location.replace("./docs/overview"));
 
-  return <Layout title={`${siteConfig.title}`} description="Visualiser" />;
+  return (
+    <Layout title="Home" description="Pathfinding Visualiser Documentation" />
+  );
 }
